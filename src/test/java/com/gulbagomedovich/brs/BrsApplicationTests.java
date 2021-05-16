8cbc3bd04a1a2e1368f469fa02ecdbafbac4836e
gulbagomedovich <gulbagomedovich@gmail.com>
2021-05-16 17:13:14 +0300

    Initial commit

---
+++ src/test/java/com/gulbagomedovich/brs/BrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbagomedovich.brs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
